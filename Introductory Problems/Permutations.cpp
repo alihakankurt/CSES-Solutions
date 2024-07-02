6a6420deb24ef024c6dab72c719eba10d8962b81
alihakankurt <kurtalihakan@outlook.com>
2024-07-02 23:28:58 +0300

    Add `Permutations` solution

---
+++ Introductory Problems/Permutations.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = ptrdiff_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float_t;
+using f64 = double_t;
+
+inline constexpr i32 Modulus = 1e9 + 7;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    i32 n;
+    cin >> n;
+
+    if (n == 2 || n == 3)
+    {
+        cout << "NO SOLUTION";
+        return 0;
+    }
+
+    for (i32 num = 2; num <= n; num += 2)
+    {
+        cout << num << ' ';
+    }
+
+    for (i32 num = 1; num <= n; num += 2)
+    {
+        cout << num << ' ';
+    }
+
+    return 0;
+}
