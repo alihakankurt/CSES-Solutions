ae5caa764f8bb066d900bc04cddcb0bf701378c4
alihakankurt <kurtalihakan@outlook.com>
2024-07-02 23:10:17 +0300

    Add `Missing Number` solution

---
+++ Introductory Problems/Missing Number.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = ptrdiff_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float_t;
+using f64 = double_t;
+
+inline constexpr i32 Modulus = 1e9 + 7;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    i32 n;
+    cin >> n;
+
+    i32 value = n;
+    for (i32 index = 1; index < n; index += 1)
+    {
+        i32 num;
+        cin >> num;
+        value = value ^ num ^ index;
+    }
+
+    cout << value;
+
+    return 0;
+}
