332639faf80190d3bb0108f9e7d87d41869f1c70
alihakankurt <kurtalihakan@outlook.com>
2024-07-03 14:07:19 +0300

    Add `Coin Piles` solution

---
+++ Introductory Problems/Coin Piles.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = ptrdiff_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float_t;
+using f64 = double_t;
+
+inline constexpr i32 Modulus = 1e9 + 7;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    usize t;
+    cin >> t;
+
+    while (t > 0)
+    {
+        t -= 1;
+
+        u64 a, b;
+        cin >> a >> b;
+
+        u64 sum = a + b;
+        u64 moves = sum / 3;
+
+        if (moves * 3 == sum && a >= moves && b >= moves)
+        {
+            cout << "YES" << '\n';
+        }
+        else
+        {
+            cout << "NO" << '\n';
+        }
+    }
+
+    return 0;
+}
