d60bf407d4e68023757639d7475e78ac1f4cf6f1
alihakankurt <kurtalihakan@outlook.com>
2025-04-26 17:56:34 +0300

    Add `Subarray Divisibility` solution

---
+++ Sorting and Searching/Subarray Divisibility.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = ptrdiff_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float_t;
+using f64 = double_t;
+
+inline constexpr i32 Modulus = 1e9 + 7;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    usize n;
+    cin >> n;
+
+    usize count = 0;
+    i64 remainder = 0;
+    map<i64, usize> remainderCounts;
+    remainderCounts[0] = 1;
+
+    for (usize index = 0; index < n; index += 1)
+    {
+        i32 num;
+        cin >> num;
+        remainder += num;
+        remainder = ((remainder < 0) ? n - (-remainder % n) : remainder) % n;
+        count += remainderCounts[remainder];
+        remainderCounts[remainder] += 1;
+    }
+
+    cout << count;
+
+    return 0;
+}
