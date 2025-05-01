8fc3b89ff230c6619eea8556fbb418037fc5d51c
alihakankurt <kurtalihakan@outlook.com>
2025-05-01 18:24:10 +0300

    Add `Dice Combinations` solution

---
+++ Dynamic Programming/Dice Combinations.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = ptrdiff_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float_t;
+using f64 = double_t;
+
+inline constexpr i32 Modulus = 1e9 + 7;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    usize n;
+    cin >> n;
+
+    vector<u64> dp(n + 1);
+    dp[0] = 1;
+
+    for (usize sum = 1; sum <= n; sum += 1)
+    {
+        for (u64 dice = 1; dice <= 6 && dice <= sum; dice += 1)
+        {
+            dp[sum] += dp[sum - dice];
+        }
+
+        dp[sum] %= Modulus;
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
