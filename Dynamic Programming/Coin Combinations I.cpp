b96d9678b2cf6ce97104ba643d440e8e4909d68b
alihakankurt <kurtalihakan@outlook.com>
2025-05-02 14:10:23 +0300

    Add `Coin Combinations I` solution

---
+++ Dynamic Programming/Coin Combinations I.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = ptrdiff_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float_t;
+using f64 = double_t;
+
+inline constexpr i32 Modulus = 1e9 + 7;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    usize n;
+    cin >> n;
+
+    u32 x;
+    cin >> x;
+
+    vector<u32> coins(n);
+    for (usize index = 0; index < n; index += 1)
+    {
+        cin >> coins[index];
+    }
+
+    vector<u32> dp(x + 1, 0);
+    dp[0] = 1;
+
+    for (u32 sum = 1; sum <= x; sum += 1)
+    {
+        for (const u32& coin : coins)
+        {
+            if (coin <= sum)
+            {
+                dp[sum] = (dp[sum] + dp[sum - coin]) % Modulus;
+            }
+        }
+    }
+
+    cout << dp[x];
+
+    return 0;
+}
