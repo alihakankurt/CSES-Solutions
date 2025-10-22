84d8a748e9df15a71a9cff0140cba0a77d0d3ad2
alihakankurt <kurtalihakan@outlook.com>
2025-10-22 21:51:52 +0300

    Add `Grid Coloring I` solution

---
+++ Introductory Problems/Grid Coloring I.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = ptrdiff_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float_t;
+using f64 = double_t;
+
+inline constexpr i32 Modulus = 1e9 + 7;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    u16 n, m;
+    cin >> n >> m;
+
+    auto grid = vector<string>(n);
+    for (auto& row : grid)
+    {
+        cin >> row;
+    }
+
+    for (u16 y = 0; y < n; y += 1)
+    {
+        for (u16 x = 0; x < m; x += 1)
+        {
+            char& color = grid[y][x];
+            u16 parity = (y + x) & 0b1;
+
+            char targetColor = 'A' + parity;
+            targetColor += (targetColor == color) << 1;
+
+            color = targetColor;
+        }
+    }
+
+    for (auto& row : grid)
+    {
+        cout << row << '\n';
+    }
+
+    return 0;
+}
