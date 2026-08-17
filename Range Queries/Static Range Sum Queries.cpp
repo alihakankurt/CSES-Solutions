5b0515222de22d94d2c66d0a6f3bdd81ee55f98a
alihakankurt <kurtalihakan@outlook.com>
2026-08-17 23:07:32 +0300

    Add `Static Range Sum Queries` solution

---
+++ Range Queries/Static Range Sum Queries.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using uptr = uintptr_t;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = make_signed_t<size_t>;
+using iptr = intptr_t;
+
+using f32 = float_t;
+using f64 = double_t;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    usize valuesLength, queriesLength;
+    cin >> valuesLength >> queriesLength;
+
+    auto prefixSum = vector<u64>(valuesLength + 1u);
+    for (usize index = 1u; index <= valuesLength; index += 1u)
+    {
+        cin >> prefixSum[index];
+        prefixSum[index] += prefixSum[index - 1u];
+    }
+
+    for (usize _ = 1u; _ <= queriesLength; _ += 1u)
+    {
+        usize left, right;
+        cin >> left >> right;
+        cout << prefixSum[right] - prefixSum[left - 1u] << '\n';
+    }
+
+    return 0;
+}
